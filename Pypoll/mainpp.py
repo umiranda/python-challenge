16b35c7a068529e6ef5fc1044e82c35c50aa05c8
umiranda <71298635+umiranda@users.noreply.github.com>
2020-10-17 12:23:54 -0400

    Update mainpp.py

--- Pypoll/mainpp.py
+++ Pypoll/mainpp.py
@@ -1,18 +1,23 @@
 
+
+#import dependencies
 import csv
 import os
 
+#retrieving data from file
 filepath = os.path.join("Resources", "Unit 3 - Python_Homework_Instructions_PyPoll_Resources_election_data.csv")
 
+#reading file
 with open(filepath, 'r') as input_file:
     csvreader = csv.reader(input_file,delimiter=",")
 
+#declaring variables
     header = next(csvreader)
     Voter_ID = []
     County = []
     Candidate = []
     Candidates = {}
-    
+    #looping through to extract results
     for row in csvreader:
         Voter_ID.append(row[0])
         Candidate.append(row[2])
@@ -21,7 +26,7 @@ with open(filepath, 'r') as input_file:
             Candidates[row[2]] +=1
         else:
             Candidates[row[2]] = 1
-
+    #write and create an output txt
     with open("pp_output.txt", "w") as datafile:
 
         election_responses = (
@@ -33,7 +38,7 @@ with open(filepath, 'r') as input_file:
 
         print(election_responses)
         datafile.write(election_responses)
-
+        #looping to find out and write information for each candidate
         for candidate_name in Candidates:
 
             vote_amount = Candidates.get(candidate_name)
@@ -41,7 +46,7 @@ with open(filepath, 'r') as input_file:
             voter_output = f"{candidate_name} : {voter_percentage:.3f}% ({vote_amount})\n"
             datafile.write(voter_output)
             print(voter_output)
-
+        #getting and writting the election winner
         winning_name = max(Candidates,key=Candidates.get)
 
         winning_output = (
